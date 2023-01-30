e974a536232daa16f3666f8a9b317b5d1225fd2a
joaooliveira247 <joaooliveira247@gmail.com>
2023-01-30 11:50:32 -0300

    :sparkles: add print and variables

---
+++ Projetos/Python/print_n_variables.py
@@ -0,0 +1,8 @@
+print("O brasil tem cinco copas do mundo!")
+
+print("O", "brasil", "tem", "cinco", "copas", "do", "mundo", "!", sep="_")
+
+pais = "argentina"
+pais_copa = 3
+
+print("A", pais, "tem", pais_copa, "copas", "do", "mundo", sep=" ", end="!\n")
