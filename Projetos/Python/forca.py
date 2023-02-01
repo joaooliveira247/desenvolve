93fc27002a5c74104e61259d8a78b62d7fd78ecf
joaooliveira247 <joaooliveira247@gmail.com>
2023-02-01 17:09:26 -0300

    :sparkles: add jogo da forca

---
+++ Projetos/Python/forca.py
@@ -0,0 +1,16 @@
+def jogo():
+    print("Jogo da forca")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        print("Jogando ...")
+
+    print("Fim do Jogo")
+
+
+if __name__ == "__main__":
+    jogo()
