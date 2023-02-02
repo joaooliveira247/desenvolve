c3778e9187661525d0a9bcf029caa5e389f32299
joaooliveira247 <joaooliveira247@gmail.com>
2023-02-02 19:06:31 -0300

    :sparkles: finish

--- Projetos/Python/forca.py
+++ Projetos/Python/forca.py
@@ -1,12 +1,24 @@
+from random import randrange
+
+
+def read_words(path):
+    with open(path, "r", encoding="UTF-8") as archive:
+        return [x.strip().lower() for x in archive.readlines()]
+
+
+def raffle_word(path):
+    words = read_words(path)
+    return words[randrange(0, len(words))]
+
+
 def jogo():
     print("Jogo da forca")
 
-    palavra_secreta = "banana"
+    palavra_secreta = raffle_word("./words.txt")
+    letras_acertadas = ["_"] * len(palavra_secreta)
+    tryes = 0
 
-    enforcou = False
-    acertou = False
-
-    while (not enforcou and not acertou):
+    while not tryes > (len(palavra_secreta) - 1):
 
         chute = input("Qual letra ?\n")
         chute = chute.strip()
@@ -14,13 +26,17 @@ def jogo():
         index = 0
 
         for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                print(f"Letra {letra} encontrada na posição {index}")
+            if chute.lower() == letra.lower():
+                letras_acertadas[index] = letra
             index += 1
+        tryes += 1
+        palavra_parcial = "".join(letras_acertadas)
+        print(palavra_parcial)
+        if palavra_parcial == palavra_secreta:
+            print("You Win")
+            break
 
-        print("Jogando ...")
-
-    print("Fim do Jogo")
+    print("Parece que não foi dessa vez")
 
 
 if __name__ == "__main__":
